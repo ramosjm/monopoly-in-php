57a5c23493c44505b6ee5e136aad89271bfe9b8e
Janapoles Ramos <janapolesramos@janapoles-mbp.mynet>
2021-02-06 18:21:37 +0000

    Added a tiles class to generate all types of tiles.

---
+++ models/tiles.php
@@ -0,0 +1,53 @@
+<?php 
+
+include_once('models/tile-free.php');
+include_once('models/tile-tax.php');
+include_once('models/tile-chance.php');
+include_once('models/tile-transport.php');
+include_once('models/tile-utility.php');
+include_once('models/tile-chest.php');
+include_once('models/tile-property.php');
+
+	class Tiles {
+
+		private $tiles;
+
+		public function __construct(){
+			$this->tiles = $this->generateTiles();
+		}
+
+		public function generateTiles(){
+			$data = TileHelper::getData();
+			$tiles = array();
+			foreach ($data as $tile){
+
+				switch ($tile->type) {
+					case 'free':
+						$tiles[] = new TileFree($tile);
+						break;
+					case 'tax':
+						$tiles[] =  new TileTax($tile);
+						break;
+					case 'chance':
+						$tiles[] =  new TileChance($tile);
+						break;
+					case 'transport':
+						$tiles[] =  new TileTransport($tile);
+						break;
+					case 'utility':
+						$tiles[] =  new TileUtility($tile);
+						break;
+					case 'chest':
+						$tiles[] =  new TileChest($tile);
+						break;
+					default:
+						$tiles[] =  new TileProperty($tile);
+						break;
+				}
+			}
+			return $tiles;
+
+		}
+	}
+
+?>
