6c31f5e0252c30a4e741933e44edad08ab747b02
Janapoles Ramos <janapolesramos@janapoles-mbp.mynet>
2021-02-07 17:58:11 +0000

    set house_count to null in model.

--- models/tile-property.php
+++ models/tile-property.php
@@ -8,7 +8,7 @@
 			parent::__construct($tile);
 			$this->house_cost = $tile->housecost;
 			$this->hotel_cost = $tile->hotelcost;
-			$this->house_count = 0;
+			$this->house_count = null;
 		}
 
 		// for now this will add 1 to the house count -- consider expanding this to allowe user to choose number of houses
