d02896ea1e2e876b1e72b182a3fd5e7153c728f1
Janapoles Ramos <janapolesramos@janapoles-mbp.mynet>
2021-02-07 18:05:30 +0000

    Added basic controller functionality.

---
+++ controller/controller.php
@@ -0,0 +1,20 @@
+<?php 
+
+// controller will need listeners to check for changes in the view.
+
+	class Controller{
+		private $tiles;
+		private $view;
+
+		public function __construct($tiles, $view){
+			$this->tiles = $tiles;
+			$this->view = $view;
+		}
+		
+		public function main(){
+			$this->view->renderTiles($this->tiles);
+		}
+		
+	}
+
+?>
