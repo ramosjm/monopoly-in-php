ea950be52e8b771a3f2019e165cb86634008cf04
Janapoles Ramos <janapolesramos@janapoles-mbp.mynet>
2021-02-07 18:04:25 +0000

    Added index.php and calls to the controller.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "hello"?>
+<?php
+
+require_once('controller/controller.php');
+require_once('models/tiles.php');
+require_once('views/board-view.php');
+require_once('helpers/tile-helper.php');
+include ('includes/header.php');
+
+//Create teh controller passing in the model and the view
+
+$tiles = new Tiles();
+$tile_list = $tiles->generateTiles();
+$board_view = new BoardView();
+$controller = new Controller($tile_list, $board_view);
+
+// Main will start the render process
+$controller->main();
+
+include 'includes/footer.php';
+?>
