c431b2d17d03e78b24a0c215c41a537f592eede9
Janapoles Ramos <janapolesramos@janapoles-mbp.mynet>
2021-02-09 22:45:11 +0000

    Rendering board with some hella basic css.

--- views/board-view.php
+++ views/board-view.php
@@ -7,38 +7,34 @@
 			// need to create a div to keep all the tiles inside it
 			// all individual tiles need to have their own divs -- add this to the tile view class
 			$board_element = '<div class="board">';
+			$top = '<div class="flex-top">';
+			$bottom = '<div class="flex-bottom">';
+			$mid_section = '<div class="mid-section">';
+			$left = '<div class="flex-left">';
+			$right = '<div class="flex-right">';
+			$center = '<div class="center">';
 			foreach ($tiles as $tile) {
-				$flex_element = $this->setFlexClass($tile->id);
-				$board_element .= $flex_element;
-				$tile_view = new TileView();
-				$board_element .= $tile_view->renderTile($tile);
-				if($tile->id == 10 || $tile->id == 20 || $tile->id == 30 || $tile->id == 40){
-					$board_element .= '</div>';
-				}
-			}
-			$board_element .= "</div>";
-			echo $board_element;
-		}
 
-		public function setFlexClass($id){
-			$flex_div="";
-			switch ($id) {
-				case $id == 1:
-					$flex_div='<div class="flex-top">';
-					break;
-				case $id == 11:
-					$flex_div='<div class="flex-right">';
-					break;
-				case $id == 21:
-					$flex_div='<div class="flex-bottom">';
-					break;
-				case $id == 31:
-					$flex_div='<div class="flex-left">';
-					break;
-				default:
-					break;
+				$tile_view = new TileView();
+				$tile_html = $tile_view->renderTile($tile);
+				if($tile->id <= 11){
+					$bottom .= $tile_html;
+				}
+				if($tile->id >= 12 && $tile->id <= 20) {
+					$left .= $tile_html;
+				} elseif ($tile->id >= 32 && $tile->id <= 40) {
+					$right .= $tile_html;
+				}
+
+				if($tile->id >= 21 && $tile->id <= 31){
+					$top .= $tile_html;
+				}
+
 			}
-			return $flex_div;
+			$mid_section .= $left . '</div>' . $center . '</div>' . $right . '</div>';
+			$board_element .= $bottom . '</div>' . $mid_section . '</div>' . $top . '</div>' . '</div>';
+
+			echo $board_element;
 		}
 	}
 
